a61f37a1c469ee71085630ccafcef715b9721d17
joelvilcav <jvilcav29@gmail.com>
2023-11-08 15:45:55 -0500

    View to set a new password and confirm accounts added

--- src/pages/ConfirmAccount.jsx
+++ src/pages/ConfirmAccount.jsx
@@ -1,7 +1,11 @@
 const ConfirmAccount = () => {
   return (
-    <div>ConfirmAccount</div>
-  )
-}
+    <>
+      <h1 className='text-sky-600 font-black text-6xl capitalize'>
+        Confirm your <span className='text-slate-700'>account</span>
+      </h1>
+    </>
+  );
+};
 
-export default ConfirmAccount
+export default ConfirmAccount;
--- src/pages/NewPassword.jsx
+++ src/pages/NewPassword.jsx
@@ -1,7 +1,35 @@
 const NewPassword = () => {
   return (
-    <div>NewPassword</div>
-  )
-}
+    <>
+      <h1 className='text-sky-600 font-black text-6xl capitalize'>
+        Reset your and get access to your{' '}
+        <span className='text-slate-700'>account</span>
+      </h1>
 
-export default NewPassword
+      <form className='mt-10 mb-6 bg-white shadow rounded-lg px-8 py-6'>
+        <div>
+          <label
+            className='text-gray-600 block text-md font-bold'
+            htmlFor='password'
+          >
+            New password
+          </label>
+          <input
+            className='w-full mt-2 p-3 border rounded-xl bg-gray-50'
+            type='password'
+            placeholder='Write your new password'
+            id='password'
+          />
+        </div>
+
+        <input
+          type='submit'
+          value='Save New Password'
+          className='bg-sky-700 w-full mt-3 py-2 text-white font-bold rounded hover:cursor-pointer hover:bg-sky-800 transition-colors'
+        />
+      </form>
+    </>
+  );
+};
+
+export default NewPassword;
