5064402154ac2e6bad6edcaa50e64ab682248830
joelvilcav <jvilcav29@gmail.com>
2023-11-08 12:37:01 -0500

    Login page implemented

--- src/layouts/AuthLayout.jsx
+++ src/layouts/AuthLayout.jsx
@@ -3,8 +3,11 @@ import { Outlet } from 'react-router-dom';
 const AuthLayout = () => {
   return (
     <>
-      <div>AuthLayout</div>
-      <Outlet />
+      <main className='container mx-auto p-5 flex justify-center items-center h-screen md:flex md:justify-center'>
+        <div className='md:w-2/3 lg:w-1/3'>
+          <Outlet />
+        </div>
+      </main>
     </>
   );
 };
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,49 @@
 const Login = () => {
   return (
-    <div>Login</div>
-  )
-}
+    <>
+      <h1 className='text-sky-600 font-black text-6xl capitalize'>
+        Get in and manage your <span className='text-slate-700'>projects</span>
+      </h1>
 
-export default Login
+      <form className='my-10 bg-white shadow rounded-lg px-8 py-6'>
+        <div>
+          <label
+            className='text-gray-600 block text-md font-bold'
+            htmlFor='email'
+          >
+            Email
+          </label>
+          <input
+            className='w-full mt-2 p-3 border rounded-xl bg-gray-50'
+            type='email'
+            placeholder='email@email.com'
+            id='email'
+          />
+        </div>
+
+        <div>
+          <label
+            className='text-gray-600 block text-md font-bold'
+            htmlFor='password'
+          >
+            Password
+          </label>
+          <input
+            className='w-full mt-2 p-3 border rounded-xl bg-gray-50'
+            type='password'
+            placeholder='Your password'
+            id='password'
+          />
+
+          <input
+            type='submit'
+            value='Log In'
+            className='bg-sky-700 w-full mt-3 py-2 text-white font-bold rounded hover:cursor-pointer hover:bg-sky-800 transition-colors'
+          />
+        </div>
+      </form>
+    </>
+  );
+};
+
+export default Login;
