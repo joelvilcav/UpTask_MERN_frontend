aab8b048abb00dd4cf726ce7d93dffaefbea4ee3
joelvilcav <jvilcav29@gmail.com>
2023-11-14 15:20:24 -0500

    Header and sidebar created, and added to protected route layout

--- src/layouts/ProtectedRoute.jsx
+++ src/layouts/ProtectedRoute.jsx
@@ -1,10 +1,29 @@
 import { Outlet, Navigate } from 'react-router-dom';
 import useAuth from '../hooks/useAuth';
 
+import Sidebar from '../components/Sidebar';
+import Header from '../components/Header';
+
 const ProtectedRoute = () => {
   const { auth, loading } = useAuth();
   if (loading) return 'Loading...';
-  return <>{auth._id ? <Outlet /> : <Navigate to='/' />}</>;
+  return (
+    <>
+      {auth._id ? (
+        <div className='bg-gray-100'>
+          <Header />
+          <div className='md:flex md:min-h-screen'>
+            <Sidebar />
+            <main>
+              <Outlet />
+            </main>
+          </div>
+        </div>
+      ) : (
+        <Navigate to='/' />
+      )}
+    </>
+  );
 };
 
 export default ProtectedRoute;
