d06940a79b1f692076ea54854dd24c62a84dc38e
joelvilcav <jvilcav29@gmail.com>
2023-11-14 15:46:55 -0500

    Call AuthContext to the sidebar to get user info

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,14 +1,20 @@
-import { Link } from "react-router-dom"
+import { Link } from 'react-router-dom';
+import useAuth from '../hooks/useAuth';
 
 const Sidebar = () => {
+  const { auth } = useAuth();
+  console.log(auth);
   return (
-    <aside className="md:w-80 lg:w-96 px-5 py-10">
-        <p className="text-xl font-bold">Hi there: Joel</p>
-        <Link to="create-project" className="bg-sky-600 p-3 text-white font-bold block mt-5 text-center rounded-lg">
-            New Project
-        </Link>
+    <aside className='md:w-80 lg:w-96 px-5 py-10'>
+      <p className='text-xl font-bold'>Hi there: Joel</p>
+      <Link
+        to='create-project'
+        className='bg-sky-600 p-3 text-white font-bold block mt-5 text-center rounded-lg'
+      >
+        New Project
+      </Link>
     </aside>
-  )
-}
+  );
+};
 
-export default Sidebar
+export default Sidebar;
