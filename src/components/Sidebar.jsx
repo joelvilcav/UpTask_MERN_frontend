d49f132abf5ee89a1549646bd58dc1254d31c865
joelvilcav <jvilcav29@gmail.com>
2023-11-14 15:29:28 -0500

    Styles added to sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,6 +1,13 @@
+import { Link } from "react-router-dom"
+
 const Sidebar = () => {
   return (
-    <div>Sidebar</div>
+    <aside className="md:w-80 lg:w-96 px-5 py-10">
+        <p className="text-xl font-bold">Hi there: Joel</p>
+        <Link to="create-project" className="bg-sky-600 p-3 text-white font-bold block mt-5 text-center rounded-lg">
+            New Project
+        </Link>
+    </aside>
   )
 }
 
