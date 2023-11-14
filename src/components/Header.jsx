e4cb66f00f428bb7a3471a8ca16a107e42b384fd
joelvilcav <jvilcav29@gmail.com>
2023-11-14 15:25:08 -0500

    Styles added to the header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,31 @@
+import { Link } from 'react-router-dom';
+
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <header className='px-4 py-4 bg-white border-b'>
+      <div className='md:flex md:justify-between'>
+        <h2 className='text-4xl text-sky-600 font-black text-center'>UpTask</h2>
 
-export default Header
+        <input
+          type='search'
+          placeholder='Search project'
+          className='rounded-lg lg:w-96 block p-2 border'
+        />
+
+        <div className='flex items-center gap-4'>
+          <Link to='/projects' className='font-bold uppercase'>
+            Projects
+          </Link>
+          <button
+            type='button'
+            className='text-white text-sm bg-sky-600 p-3 rounded-md font-bold'
+          >
+            Log out
+          </button>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
