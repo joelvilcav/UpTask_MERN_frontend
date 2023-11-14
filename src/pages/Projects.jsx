54c6a59bcc76b624369adce60b84e72b08004837
joelvilcav <jvilcav29@gmail.com>
2023-11-14 16:05:11 -0500

    Hook to access project context

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,12 +1,15 @@
+import useProjects from '../hooks/useProjects';
+
 const Projects = () => {
+  const { projects } = useProjects();
+  console.log(projects);
+  
   return (
     <>
-      <h1 className="text-4xl font-black">Projects</h1>
-      <div>
-        
-      </div>
+      <h1 className='text-4xl font-black'>Projects</h1>
+      <div></div>
     </>
-  )
-}
+  );
+};
 
-export default Projects
+export default Projects;
