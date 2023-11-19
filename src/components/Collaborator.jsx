430e2e617cd370f37610ccef39cd127bfc628fcb
joelvilcav <jvilcav29@gmail.com>
2023-11-19 17:26:56 -0500

    Showing collaborator info

--- src/components/Collaborator.jsx
+++ src/components/Collaborator.jsx
@@ -1,7 +1,21 @@
-const Collaborator = () => {
+const Collaborator = ({ collaborator }) => {
+  const { name, email } = collaborator;
   return (
-    <div>Collaborator</div>
-  )
-}
+    <div className='border-b p-5 flex justify-between items-center'>
+      <div>
+        <p>{name}</p>
+        <p className='text-sm text-gray-700'>{email}</p>
+      </div>
+      <div>
+        <button
+          type='button'
+          className='bg-red-600 px-4 py-3 text-white text-sm rounded-lg'
+        >
+          Delete
+        </button>
+      </div>
+    </div>
+  );
+};
 
-export default Collaborator
+export default Collaborator;
