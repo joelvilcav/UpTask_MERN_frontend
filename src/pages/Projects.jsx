714b7d1314db498c3b44d80a4d6806ce7d0a7fcb
joelvilcav <jvilcav29@gmail.com>
2023-11-20 12:05:05 -0500

    Socket io client installed and format code done

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -10,14 +10,16 @@ const Projects = () => {
   return (
     <>
       <h1 className='text-4xl font-black'>Projects</h1>
-      { msg && <Alert alert={alert}/>}
+      {msg && <Alert alert={alert} />}
       <div className='bg-white shadow mt-10 rounded-lg'>
         {projects.length ? (
           projects.map((project) => (
             <PreviewProject key={project._id} project={project} />
           ))
         ) : (
-          <p className='text-center text-gray-600 uppercase p-5'>No projects yet</p>
+          <p className='text-center text-gray-600 uppercase p-5'>
+            No projects yet
+          </p>
         )}
       </div>
     </>
